359b8701d715b6354e3c35eb811f372ca4c829d5
crrashh1542 <junzhu12345@qq.com>
2022-10-02 18:25:21 +0800

    规范化，引入了 Vue

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
-import './styles/index.less'
-import './styles/different-displays.less'
+// 引入样式
+import './styles/index/index.less'
+import './styles/index/different-displays.less'
+
+Vue.config.productionTip = false; // 阻止 Vue 提示切换到生产模式
+
+function avatarDir(name) {
+    return "https://www.crrashh1542.top/img/avatars/" + name + ".jpg";
+}
+const vm = new Vue({
+    el: "#index-app",
+    data: function(){
+        return {
+            friends: [
+                {
+                    name: "Gaoice !喵",
+                    link: "https://gaoice.top",
+                    intro: "传说中的某大佬",
+                    avatar: avatarDir("gaoice")
+                },
+                {
+                    name: "bai",
+                    link: "https://baizhi.fun",
+                    intro: "一个安静的学习站",
+                    avatar: avatarDir("bai")
+                }
+            ],
+            links: ["http://wpa.qq.com/msgrd?v=3&uin=3247380086&site=qq&menu=yes", "mailto:junzhu12345@qq.com", "https://github.com/crrashh1542"]
+        }
+    }
+});
--- src/main.js
+++ src/main.js
@@ -1,23 +1,49 @@
-// 引入库
+/* imports */
+// 引入 js
 import sayAge from './js/birthday' // 此部分代码来自 Gaoice
 // 引入样式
-import './styles/reset.less'
+import './styles/reset/global.less'
+import './styles/reset/components.less'
+import './styles/reset/content.less'
 
+/* functions */
 // 1. 年龄显示
 sayAge("2007-3-18");
+
 // 2. mdui 抽屉栏初始化
 var $ = mdui.$;
 var drawer = new mdui.Drawer("#drawer");
 $('#drawer-btn').on('click', function () {
     drawer.toggle();
 });
+
 // 3. mdui 对话框初始化
 $('#about').on('click', function () {
     mdui.dialog({
-        title: "关于本网站",
-        content: "本网站由云萧（@crrashh1542）自己搭建。<br>部分使用 MDUI 进行 Material 化设计。<br>采用 Webpack 5 打包（但是没有其他框架，因为我菜qwq）<br>此网站代码开源，详见 Github@crrashh1542",
+        title: "关于本站",
+        content: "本站由云萧（@crrashh1542）自己搭建。<br>部分使用 MDUI 进行 Material 化设计，采用 Webpack 5 打包<br>本网站的 Vue 化已经提上日程！<br>此网站代码详见 Github@crrashh1542",
         buttons: [{
             text: "了解"
         }]
     })
 });
+
+// 4. 设置 Vue 对象
+Vue.config.productionTip = false
+// 设置检测模块，如果网页中有 index-app 则不启用默认的
+window.onload = function() {
+    if($("#app") !== null){
+        console.log("No specific app.");
+        // 正片开始
+        const vm = new Vue({
+            el: "#app",
+            data: function(){
+                return {
+                    links: ["http://wpa.qq.com/msgrd?v=3&uin=3247380086&site=qq&menu=yes", "mailto:junzhu12345@qq.com", "https://github.com/crrashh1542"]
+                }
+            }
+        });
+    } else {
+        console.log("A specific app already exists.");
+    }
+}

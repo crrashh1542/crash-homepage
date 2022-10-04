4c294a2fcb155416e3f3c1e221956ab1e7a1b645
crrashh1542 <junzhu12345@qq.com>
2022-10-04 18:25:31 +0800

    优化结构

--- src/1.js
+++ src/1.js
@@ -1,28 +1,72 @@
 // This is served for friends/index.html
+// You can also take it as a friend list.
 
 const avatarDir = (name) => "https://www.crrashh1542.top/img/avatars/" + name + ".jpg" // 把名字换成路径
 new Vue({
-    el: "#links",
-    data: {
-        friends: [
+    "el": "#links",
+    "data": {
+        "friends": [
             {
-                name: "Gaoice !喵",
-                link: "https://gaoice.top",
-                intro: "传说中的某大佬",
-                avatar: avatarDir("gaoice")
+                "link": "https://gaoice.top",
+                "avatar": avatarDir("gaoice"),
+                "name": "Gaoice !喵",
+                "intro": "传说中的某大佬"
+                
             },
             {
-                name: "bai",
-                link: "https://baizhi.fun",
-                intro: "一个安静的学习站",
-                avatar: avatarDir("bai")
+                "link": "https://baizhi.fun",
+                "avatar": avatarDir("bai"),
+                "name": "bai",
+                "intro": "一个安静的学习站"
             },
             {
-                name: "LCZBlog",
-                link: "https://blog.licaoz.com",
-                intro: "寄蜉蝣於天地，渺滄海之一粟。",
-                avatar: "https://source.licaoz.com/images/pubicon.png"
-            }
+                "link": "https://blog.licaoz.com",
+                "avatar": "https://source.licaoz.com/images/pubicon.png",
+                "name": "LCZBlog",
+                "intro": "寄蜉蝣於天地，渺滄海之一粟。"
+            },
+            {
+                "link": "https://tutime.cn/",
+                "avatar": avatarDir("tutime"),
+                "name": "土土博客",
+                "intro": "追逐幻想"
+            },
+            {
+                "link": "https://www.evanluo.top/",
+                "avatar": "https://evan.beee.top/avatar.png",
+                "name": "Anonymous Land",
+                "intro": "Evan's blog. Why not take a visit?"
+            },
+            {
+                "link": "",
+                "avatar": "",
+                "name": "",
+                "intro": ""
+            },
+            {
+                "link": "",
+                "avatar": "",
+                "name": "",
+                "intro": ""
+            },
+            {
+                "link": "",
+                "avatar": "",
+                "name": "",
+                "intro": ""
+            },
+            {
+                "link": "",
+                "avatar": "",
+                "name": "",
+                "intro": ""
+            },
+            {
+                "link": "",
+                "avatar": "",
+                "name": "",
+                "intro": ""
+            },
         ]
     }
 })

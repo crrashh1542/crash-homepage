2617ed322dcc757b5c897c05527ffa12a3e0ba3f
crrashh1542 <junzhu12345@qq.com>
2022-11-17 18:15:30 +0800

    Change avatar URL

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -7,7 +7,7 @@
 <template>
     <div class="introduction mdui-row">
             <div class="avatar mdui-col-lg-3 mdui-col-md-4">
-                <img src="https://www.crrashh.cn/img/avatars/crrashh1542.jpg">
+                <img src="https://res.crrashh.cn/img/avatars/crrashh1542.jpg">
             </div>
             <div class="info mdui-col-lg-9 mdui-col-md-8">
                 <div class="title">云萧的小破站ヾ(ﾟ∀ﾟゞ)</div>

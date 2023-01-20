c010aa99362d6e5c0fe37b76cbf63cbc30a3387c
crrashh1542 <junzhu12345@qq.com>
2023-01-20 15:46:26 +0800

    todo

--- src/components/contents/Proterties.vue
+++ src/components/contents/Proterties.vue
@@ -1,34 +1,42 @@
 <script>
 export default {
-    name: "BodyProterties"
+    name: "BodyProterties",
+    data(){
+        return {
+            proterty: [
+                {
+                    "item": "智商",
+                    "value": "{width: 40%;}",
+                    "tip": "偶尔脑袋也会宕机"
+                },
+                {
+                    "item": "技术",
+                    "value": "{width: 70%;}",
+                    "tip": "有，但是不多，会的比大部分人多一点点"
+                },
+                {
+                    "item": "社恐",
+                    "value": "{width: 50%;}",
+                    "tip": "多数时候有，也有时候无"
+                },
+                {
+                    "item": "心理状态",
+                    "value": "{width: 60%;}",
+                    "tip": "抗压能力还是有的，但也有崩溃的时候"
+                }
+            ]
+        }
+    }
 }
 </script>
 
 <template>
     <div class="c-proterty mdui-row">
-        <div class="c-proterty-unit mdui-col-xs-12 mdui-col-md-5">
-            <span>技术</span>
-            <span class="mdui-progress">
-                <span class="mdui-progress-determinate" style="width: 30%;"></span>
-            </span>
-        </div>
-        <div class="c-proterty-unit mdui-col-xs-12 mdui-col-md-5">
-            <span>智商</span>
-            <span class="mdui-progress">
-                <span class="mdui-progress-determinate" style="width: 30%;"></span>
-            </span>
-        </div>
-        <div class="c-proterty-unit mdui-col-xs-12 mdui-col-md-5">
-            <span>社恐</span>
-            <span class="mdui-progress">
-                <span class="mdui-progress-determinate" style="width: 30%;"></span>
-            </span>
-        </div>
-        <div class="c-proterty-unit mdui-col-xs-12 mdui-col-md-5">
-            <span>态度</span>
-            <span class="mdui-progress">
-                <span class="mdui-progress-determinate" style="width: 30%;"></span>
-            </span>
+        <div class="c-proterty-unit mdui-col-xs-12 mdui-col-md-5" v-for="(p, index) in proterty" :key="index">
+            <div class="c-title">{{p.item}}</div>
+            <div class="mdui-progress">
+                <span class="mdui-progress-determinate" :style="p.value" mdui-tooltip=""></span>
+            </div>
         </div>
     </div>
 </template>
@@ -36,7 +44,16 @@ export default {
 <style scoped lang="less">
     .c-proterty {
         .c-proterty-unit {
-            margin: 4px 5px 4px 20px;
+            margin: 4px 5px 20px 20px;
+            .c-title {
+                font-size: 17px;
+                text-indent: .25em;
+            }
+            .mdui-progress {
+                width: 80%;
+                height: 7px;
+                margin-top: 7px;
+            }
         }
     }
     @media screen and (max-width: 600px) {

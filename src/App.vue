19d7aca8ee754315055c9cf0c557044f0e23b8d4
crrashh1542 <junzhu12345@qq.com>
2023-06-23 17:56:17 +0800

    feat: 迁移数据至 json 中

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script>
 // Crrashh1542 does not exist at all!
 
-// 引入库
 import 'mdui'
-// 引入组件
 import Appbar from './components/top/Appbar.vue' // 抽屉+顶栏
 import Banner from './components/Banner.vue' // 横幅信息栏
-import MainBody from './components/MainBody.vue' // 中间内容
+import MainBody from './components/MainBody.vue' // 内容
+
+import info from './data/siteInfo.json'
+const site = info.site
 export default {
    name: 'App',
-   components: { Appbar, Banner, MainBody }
+   components: { Appbar, Banner, MainBody },
+   mounted() {
+      document.title = site.title + site.suffix
+   }
 }
 
 // 引入样式

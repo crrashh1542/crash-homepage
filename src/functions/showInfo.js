d96758c9401060b18fcd2b801efd1a7ccd4d229c
crrashh1542 <kotsuki@crrashh.com>
2023-06-30 00:22:24 +0800

    chore(showInfo): 优化项目信息显示
    使框架信息从 package.json 动态引入
    
    crrashh1542<kotsuki@crrashh.com>

--- src/functions/showInfo.js
+++ src/functions/showInfo.js
@@ -1,21 +1,23 @@
 /**
  * 此脚本用于在控制台中显示本项目相关信息
  * @author crrashh1542
- * @version 1.0.1
+ * @version 1.1.0
  */
 
-import v from "../../package.json"
+import info from "../../package.json"
 import moment from "moment"
 
-const version = v.version
+// 获取项目和框架版本
+const repoVer = info.version
+const frameVer = info.dependencies.vue.split("^")[1]
+// 获取时间和日期
 const date =  moment(Date.now()).format("YYYYMMDD")
 const time =  moment(Date.now()).format("HHmmss")
-const output = "Version: " + version + "@" + date + "T" + time + "+0800"
+let build = "Version: " + version + "@" + date + "T" + time + "+0800"
 
 export default function showInfo(){
-    console.log("%c%s", "font-size: 14px; margin: 5px", output)
-    
+    console.log("%c%s", "font-size: 14px; margin: 5px", build)
     console.log("%c%s",
         "color: #fff; background: #42b883; padding: 5px 10px; border-radius: 4px; font-size: 14px;",
-        "Built with Vue 3 framework!")
+        "Built with Vue " + frameVer + ".")
 }

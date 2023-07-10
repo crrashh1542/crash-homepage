c33884461f66df8d3cabc9a8c81b35c8f6b061da
crrashh1542 <junzhu12345@qq.com>
2023-07-10 17:17:27 +0800

    fix: 修复 build 时间跟随系统的问题
    原方案将 moment 嵌入 showInfo 会导致时间跟随系统
    现在会在 build 之前将时间写入 /temp/build-time.json 并在 build 时调用

--- vue.config.js
+++ vue.config.js
@@ -2,13 +2,28 @@
 
 // 引入配置及插件
 const { defineConfig } = require('@vue/cli-service')
-const { resolve } = require("path")
+const { resolve } = require('path')
+const fs = require('fs')
+const moment = require('moment')
 const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
-const CompressionWebpackPlugin = require("compression-webpack-plugin")
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
 
 // 生产模式配置
 const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i
 const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV)
+
+// 获取 build 的时间并写入
+const date = moment(Date.now()).format('YYYYMMDD')
+const time = moment(Date.now()).format('HHmmss')
+const timeData = '{"t": "' + date + 'T' + time + '+0800"}'
+fs.writeFile('./temp/build-time.json', timeData, err => {
+   // 如果 build 时间写入成功，返回 null
+   if(err === null) {
+      console.log('[buildTimer] build 时间写入成功！')
+   } else {
+      console.log('[buildTimer] build 时间写入失败，详情请参阅：\n' + err)
+   }
+})
 
 module.exports = defineConfig({
 

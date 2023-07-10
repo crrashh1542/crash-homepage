ce1605e785206ffc4f3f81f5e99286e6bb13c0f1
crrashh1542 <junzhu12345@qq.com>
2023-07-10 16:50:33 +0800

    perf: 优化打包及访问性能
    * 引入speed-measure-webpack-plugin对打包速度进行调整
    * 使用Webpack的代码分割减小chunk体积（虽然成效并不大）

--- vue.config.js
+++ vue.config.js
@@ -1,33 +1,76 @@
 /* eslint-disable */
+
+// 引入配置及插件
 const { defineConfig } = require('@vue/cli-service')
 const { resolve } = require("path")
-const CompressionWebpackPlugin = require("compression-webpack-plugin");
-const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i;
-// 判断是否生产模式
-const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV);
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin')
+const CompressionWebpackPlugin = require("compression-webpack-plugin")
+
+// 生产模式配置
+const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i
+const IS_PROD = ['production', 'prod'].includes(process.env.NODE_ENV)
 
 module.exports = defineConfig({
-    transpileDependencies: true,
-    indexPath: 'index.html' ,
-    outputDir: process.env.outputDir || 'dist',
-    assetsDir: 'assets',
-    configureWebpack: config => {
-        const plugins = [];
-        if (IS_PROD) {
-            plugins.push(
-                new CompressionWebpackPlugin({
-                    filename: "[path].gz[query]",
-                    algorithm: "gzip",
-                    test: productionGzipExtensions,
-                    threshold: 10240,
-                    minRatio: 0.8
-                })
-            );
-        }
-    },
-    devServer: {
-        host: "localhost",
-        port: 8080,
-        open: false
-    }
+
+   transpileDependencies: true,
+   indexPath: 'index.html',
+   outputDir: process.env.outputDir || 'dist',
+   assetsDir: 'assets',
+
+   configureWebpack: config => {
+      // gzip 处理
+      const plugins = [];
+      if (IS_PROD) {
+         plugins.push(
+            new CompressionWebpackPlugin({
+               filename: "[path].gz[query]",
+               algorithm: "gzip",
+               test: productionGzipExtensions,
+               threshold: 10240,
+               minRatio: 0.8
+            })
+         )
+      }
+
+      // 分析各个模板编译时长
+      config.plugins.push(
+         new SpeedMeasurePlugin()
+      )
+
+   },
+
+   // chunk 拆分
+   chainWebpack: config => {
+      config.optimization.splitChunks({
+         chunks: 'all',
+         cacheGroups: {
+            vendor: {
+               name: 'vendors',
+               test: /[\\/]node_modules[\\/]/,
+               priority: 10,
+               chunks: 'initial'
+            },
+            mdui: {
+               name: 'chunks',
+               priority: 20,
+               test: /[\\/]node_modules[\\/]_?mdui(.*)/
+            },
+            commons: {
+               name: 'common',
+               test: resolve('src/components'),
+               priority: 5,
+               reuseExistingChunk: true
+            },
+         },
+      })
+      config.optimization.runtimeChunk({
+         name: 'runtime'
+      })
+   },
+
+   devServer: {
+      host: "localhost",
+      port: 8080,
+      open: false
+   }
 })

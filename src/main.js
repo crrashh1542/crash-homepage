86a27529deec64730baf1f69748b5741d5507ee1
crrashh1542 <kotsuki@crrashh.com>
2023-07-30 16:15:58 +0800

    feat: 实现生日计算器的自定义化

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import { createApp } from 'vue'
 import 'mdui/dist/js/mdui.min.js'
 import 'mdui/dist/css/mdui.min.css'
 
+// 引入配置文件
+import config from './config/config'
+const birthday = config.console.birthdayCalc.birthday
+
 // 引入 App 组件
 import App from './App.vue'
 
@@ -16,5 +20,5 @@ createApp(App).mount('#app')
 // 控制台显示奇奇怪怪的东西
 import showInfo from './functions/showInfo.js'
 showInfo()
-import sayAge from './functions/birthday.js'
-sayAge("2007-03-18")
+import sayAge from './functions/birthdayCalc.js'
+sayAge(birthday)

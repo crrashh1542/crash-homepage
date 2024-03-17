8dcd3ef862180e4231ee668b461594b6a46cc2e0
crrashh1542 <kotsuki@crrashh.com>
2024-03-18 01:58:58 +0800

    chore(alias): 添加@为src/别名，方便使用
    Signed-off-by: crrashh1542 <kotsuki@crrashh.com>

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,18 @@
 import { defineConfig } from 'astro/config'
+import { fileURLToPath, URL } from 'node:url'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+    // 引入 CSS 处理器
+    integrations: [tailwind()],
+
+    // 引入 alias
+    vite: {
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url)),
+            }
+        }
+    }
+})
